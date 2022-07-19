89d4b4e4a14e50c2f421f5b442487aa663d28fc5
flow <fl0w1337@aol.de>
2022-07-19 19:51:56 +0200

    css changes to make site less taxing on shitty devices

--- src/js/script.js
+++ src/js/script.js
@@ -8,9 +8,7 @@ const positions = [
 const rickMp3 = document.createElement('audio');
 let rickCounter = 0;
 
-const css = "font-size: 40px;color:rgb(255,105,180);font-family:monospace;letter-spacing: 5px; text-shadow: 3px 3px 20px #ff99cc, -2px 1px 30px #ff99cc;padding:20px;";
-console.log("%cWELCOME %s", css, 'TO FLOW.FAIL STRANGER.');
-console.log("%cThis is not the console you are looking for.", "color:rgb(0,0,0); font-size:20px;font-family:monospace;padding-left:20px;");
+
 
 function rickyfy() {
     const ricky = document.getElementById('ricky');
@@ -32,9 +30,14 @@ function rickyfy() {
 }
 
 function obiWaniFy() {
-    const img = document.getElementById('obiWan');
-    const dataUrl = getDataUrl(img);
-    console.log("%c      ", "font-size:167px; margin-left:20px;background:url(" + dataUrl + ") no-repeat;");
+   if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        const img = document.getElementById('obiWan');
+        const dataUrl = getDataUrl(img);
+        const css = "font-size: 40px;color:rgb(255,105,180);font-family:monospace;letter-spacing: 5px; text-shadow: 3px 3px 20px #ff99cc, -2px 1px 30px #ff99cc;padding:20px;";
+        console.log("%cWELCOME %s", css, 'TO FLOW.FAIL STRANGER.');
+        console.log("%cThis is not the console you are looking for.", "color:rgb(0,0,0); font-size:20px;font-family:monospace;padding-left:20px;");
+        console.log("%c      ", "font-size:167px; margin-left:20px;background:url(" + dataUrl + ") no-repeat;");
+   }
 }
 
 function getDataUrl(img) {
