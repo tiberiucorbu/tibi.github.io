2bb616fb9342e7d4957e125325f9da53ecd001e7
flow <fl0w1337@aol.de>
2022-07-19 22:39:02 +0200

    added quote refresh logic

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,12 @@ const positions = [
     ['0', '100%', '20px', '-50px']
 ];
 const rickMp3 = document.createElement('audio');
+let quoteBox;
+let authorBox;
+let quotes;
 let rickCounter = 0;
+let noDoubleLoading = false;
+let lastQuoteNumber;
 
 window.addEventListener('load', async (event) => {
   await getSomeMotivationalQuote();
@@ -125,17 +130,50 @@ function getRandomIntMinMax(min, max) {
 }
 
 async function getSomeMotivationalQuote() {
-     const quoteBox = document.getElementById('motivationQuote');
-     const authorBox = document.getElementById('motivationAuthor');
-     const quotes = await getQuotes();
-     const quoteNumber = Math.floor(Math.random() * quotes.length);
-     quoteBox.innerText = quotes[quoteNumber].quote;
-     authorBox.innerText = quotes[quoteNumber].author;
-     quoteBox.classList.add('fade-in');
-     authorBox.classList.add('fade-in');
+    quoteBox = document.getElementById('motivationQuote');
+    authorBox = document.getElementById('motivationAuthor');
+    quotes = await getQuotes();
+    const quoteNumber = Math.floor(Math.random() * quotes.length);
+    lastQuoteNumber = quoteNumber;
+    quoteBox.innerText = quotes[quoteNumber].quote;
+    authorBox.innerText = quotes[quoteNumber].author;
+    quoteBox.classList.add('fade-in');
+    authorBox.classList.add('fade-in');
+}
+
+async function reloadMotivationQuote() {
+    if(!noDoubleLoading) {
+        noDoubleLoading = true;
+        const button = document.getElementById('reloadButton');
+        showHide([quoteBox,authorBox,button]);
+        let quoteNumber = lastQuoteNumber;
+        while (quoteNumber===lastQuoteNumber){
+            quoteNumber =Math.floor(Math.random() * quotes.length);
+        }
+        setTimeout(() => {
+            quoteBox.innerText = quotes[quoteNumber].quote;
+            authorBox.innerText = quotes[quoteNumber].author;
+            hideShow([quoteBox,authorBox,button]);
+            noDoubleLoading = false;
+         }, 5000);
+    }
 }
 
 async function getQuotes() {
     const response = await fetch("src/media/motivations.json");
     return await response.json();
+}
+
+function showHide(array){
+        array.forEach(element => {
+                element?.classList.remove('fade-in');
+                element?.classList.add('fade-out');
+        })
+}
+
+function hideShow(array){
+        array.forEach(element => {
+                element?.classList.add('fade-in');
+                element?.classList.remove('fade-out');
+        })
 }
