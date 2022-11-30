a6a46e928211939a1dc7e675999fb34934119e11
Florian Kuenzel <florian.kuenzel@payback.net>
2022-11-30 18:47:02 +0100

    added message function

--- src/js/createMessage.js
+++ src/js/createMessage.js
@@ -1,25 +1,36 @@
 window.addEventListener('load', async (event) => {
     const button = document.getElementById('createLink');
+    const textarea = document.getElementById('message');
     button.addEventListener('click', async () => {
         createUrl();
     })
+
+    textarea.addEventListener('input', async (event) => {
+        handleResize(event);
+    })
+
+
 });
 
 
 function createUrl() {
     const headInput = document.getElementById('headMessage');
     const messageInput = document.getElementById('message');
-    const urlInput = document.getElementById('url');
     const headMessage = b64EncodeUnicode(headInput.value);
     const message = b64EncodeUnicode(messageInput.value);
     let url = new URL("https://flow.fail/msg");
     url.searchParams.append('head', headMessage);
     url.searchParams.append('msg', message);
     const urlString = url.toString();
-    urlInput.value=urlString;
     navigator.clipboard.writeText(urlString);
 }
 
 function b64EncodeUnicode(str) {
     return btoa(encodeURIComponent(str));
+}
+
+function handleResize(event) {
+    const textarea = document.getElementById('message');
+    textarea.style.height = 'auto';
+    textarea.style.height = (textarea.scrollHeight) + 'px';
 }
