41757f96afb6e69f32fcff75d6bcd3f867ea401d
flow <fl0w1337@aol.de>
2022-12-04 00:44:39 +0100

    show obi

--- src/js/script.js
+++ src/js/script.js
@@ -16,17 +16,6 @@ if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
     }, {passive: false});
 }
 
-function obiWaniFy() {
-   if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        const img = document.getElementById('obiWan');
-        const dataUrl = getDataUrl(img);
-        const css = "font-size: 40px;color:rgb(255,105,180);font-family:monospace;letter-spacing: 5px; text-shadow: 3px 3px 20px #ff99cc, -2px 1px 30px #ff99cc;padding:20px;";
-        console.log("%cWELCOME %s", css, 'TO FLOW.FAIL STRANGER.');
-        console.log("%cThis is not the console you are looking for.", "color:rgb(0,0,0); font-size:20px;font-family:monospace;padding-left:20px;");
-        console.log("%c      ", "font-size:167px; margin-left:20px;background:url(" + dataUrl + ") no-repeat;");
-   }
-}
-
 function getDataUrl(img) {
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
