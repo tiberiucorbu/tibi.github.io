5c87b85796dcbb8b1de1e4ac28665e97b884bc9c
Florian Kuenzel <florian.kuenzel@payback.net>
2023-05-04 17:51:03 +0200

    added catnip

--- src/js/uniClicker.js
+++ src/js/uniClicker.js
@@ -1,5 +1,6 @@
 let points = 0;
 let pointsPerSecond = 0;
+let iterator = 0;
 // auto clickers
 let priceNyanCat = 20;
 let amountNyanCats = 0;
@@ -15,29 +16,49 @@ let priceUltraNyanCat = 500;
 let amountUltraNyanCat = 0;
 const ultraNyanCatUpgradeCostFactor = 1.2;
 const ultraNyanCatUpgradeEfficiency = 15;
+// catnip booster
+const boosterEfficiency = 1.5;
+const priceBooster = 800;
+let amountBooster = 0;
+const boosterBaseAmount = 100;
 
-window.addEventListener('load',  (event) => {
+window.addEventListener('load', (event) => {
     update();
 });
+
 //update function
 function update() {
-    pointsPerSecond = 
-    nyanCatEfficiency * amountNyanCats + 
-    superNyanCatUpgradeEfficiency * amountSuperNyanCats + 
-    ultraNyanCatUpgradeEfficiency * amountUltraNyanCat;
+    pointsPerSecond =
+        nyanCatEfficiency * amountNyanCats +
+        superNyanCatUpgradeEfficiency * amountSuperNyanCats +
+        ultraNyanCatUpgradeEfficiency * amountUltraNyanCat;
+
+    if (amountBooster > 0) {
+        pointsPerSecond = pointsPerSecond * boosterEfficiency
+        if (iterator % 5 === 0) {
+            amountBooster = amountBooster - (amountNyanCats + +amountSuperNyanCats + amountUltraNyanCat);
+            if (amountBooster < 0) {
+                amountBooster = 0;
+            }
+        }
+    }
 
     document.getElementById('autoClickerButton').dataset.price = priceNyanCat;
-    document.getElementById('upgradeAutoClicker').dataset.price= priceSuperNyanCat;
-    document.getElementById('upgradeSuperAutoClicker').dataset.price= priceUltraNyanCat;
+    document.getElementById('upgradeAutoClicker').dataset.price = priceSuperNyanCat;
+    document.getElementById('upgradeSuperAutoClicker').dataset.price = priceUltraNyanCat;
+    document.getElementById('buyCatNip').dataset.price = priceBooster;
     updatePoints();
     updateCanPurchase();
     updatePointsPerSecond(pointsPerSecond);
     updateFields();
+
+    iterator++;
 }
 
 function generatePoints() {
     points += pointsPerSecond;
 }
+
 setInterval(update, 1000);
 setInterval(generatePoints, 1000);
 
@@ -50,28 +71,35 @@ function updatePointsPerSecond(pointsPerSecond) {
 }
 
 function updateFields() {
-    //auto clickers
-    if(amountNyanCats > 0) {
+    // auto clickers
+    if (amountNyanCats > 0) {
         document.getElementById('autoclickerCost').innerHTML = priceNyanCat;
         document.getElementById('amountAutoClicker').innerHTML = 'Nyan Cats: ' + amountNyanCats;
     } else {
         document.getElementById('amountAutoClicker').innerHTML = '';
     }
-    //upgraded clickers
+    // upgraded clickers
     document.getElementById('upgradeAutoClicker').dataset.ownedNyanCats = amountNyanCats;
-    if(amountSuperNyanCats) {
+    if (amountSuperNyanCats) {
         document.getElementById('upgradeAutoClickerCost').innerHTML = priceSuperNyanCat;
         document.getElementById('amountUpgradedClickers').innerHTML = 'Super Nyan Cats: ' + amountSuperNyanCats;
     } else {
         document.getElementById('amountUpgradedClickers').innerHTML = '';
     }
-    //super upgraded clickers
+    // super upgraded clickers
     document.getElementById('upgradeSuperAutoClicker').dataset.ownedSuperNyanCats = amountSuperNyanCats;
-    if(amountUltraNyanCat) {
+    if (amountUltraNyanCat) {
         document.getElementById('upgradeSuperAutoClickerCost').innerHTML = priceUltraNyanCat;
         document.getElementById('amountSuperUpgradedClickers').innerHTML = 'Ultra Nyan Cats: ' + amountUltraNyanCat;
     } else {
         document.getElementById('amountSuperUpgradedClickers').innerHTML = '';
+    }
+
+    // catnip
+    if (amountBooster) {
+        document.getElementById('amountCatNip').innerHTML = 'Catnip left: ' + amountBooster;
+    } else {
+        document.getElementById('amountCatNip').innerHTML = '';
     }
 }
 
@@ -81,10 +109,10 @@ function updateCanPurchase() {
             if (button.dataset.price) {
                 button.disabled = button.dataset.price > points;
             }
-            if(button.dataset.ownedNyanCats) {
+            if (button.dataset.ownedNyanCats) {
                 button.disabled = parseInt(button.dataset.ownedNyanCats) === 0 || button.dataset.price > points;
             }
-            if(button.dataset.ownedSuperNyanCats) {
+            if (button.dataset.ownedSuperNyanCats) {
                 button.disabled = parseInt(button.dataset.ownedSuperNyanCats) === 0 || button.dataset.price > points;
             }
         });
@@ -116,18 +144,18 @@ function buyAutoClicker() {
 }
 
 function upgradeAutoClicker() {
-    if(amountNyanCats > 0) {
-        if(buyItem(priceSuperNyanCat)) {
-            amountNyanCats -=1;
-            amountSuperNyanCats +=1;
+    if (amountNyanCats > 0) {
+        if (buyItem(priceSuperNyanCat)) {
+            amountNyanCats -= 1;
+            amountSuperNyanCats += 1;
             priceSuperNyanCat = Math.ceil(Math.round(priceSuperNyanCat * superNyanCatCostFactor) / 5) * 5;
             document.getElementById('upgradeAutoClickerCost').innerHTML = priceSuperNyanCat;
             let upgradedClicker = document.getElementsByClassName('autoClicker')[0];
-            if(upgradedClicker) {
+            if (upgradedClicker) {
                 const minDuration = 3;
                 const maxDuration = 6;
                 const duration = Math.random() * (maxDuration - minDuration) + minDuration;
-                upgradedClicker.classList.replace('autoClicker','autoClickerUpgrade');
+                upgradedClicker.classList.replace('autoClicker', 'autoClickerUpgrade');
                 upgradedClicker.style.setProperty('--animation-duration', `${duration}s`);
             }
         }
@@ -136,23 +164,29 @@ function upgradeAutoClicker() {
 }
 
 function upgradeSuperAutoClicker() {
-    if(amountSuperNyanCats > 0) {
-        if(buyItem(priceUltraNyanCat)) {
-            amountSuperNyanCats -=1;
-            amountUltraNyanCat +=1;
+    if (amountSuperNyanCats > 0) {
+        if (buyItem(priceUltraNyanCat)) {
+            amountSuperNyanCats -= 1;
+            amountUltraNyanCat += 1;
             priceUltraNyanCat = Math.ceil(Math.round(priceUltraNyanCat * ultraNyanCatUpgradeCostFactor) / 5) * 5;
             document.getElementById('upgradeSuperAutoClickerCost').innerHTML = priceUltraNyanCat;
             let upgradedClicker = document.getElementsByClassName('autoClickerUpgrade')[0];
-            if(upgradedClicker) {
+            if (upgradedClicker) {
                 const minDuration = 2;
                 const maxDuration = 4;
                 const duration = Math.random() * (maxDuration - minDuration) + minDuration;
-                upgradedClicker.classList.replace('autoClickerUpgrade','autoClickerSuperUpgrade');
+                upgradedClicker.classList.replace('autoClickerUpgrade', 'autoClickerSuperUpgrade');
                 upgradedClicker.style.setProperty('--animation-duration', `${duration}s`);
             }
         }
     }
 
+}
+
+function buyCatNip() {
+    if (buyItem(priceBooster)) {
+        amountBooster += boosterBaseAmount;
+    }
 }
 
 function createAutoClicker() {
