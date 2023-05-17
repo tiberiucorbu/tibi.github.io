0a1d5136ea8326d6f93a48fe67fa345d75fb1107
Tiberiu CORBU <tiberiu.corbu@gmail.com>
2023-05-17 15:43:59 +0200

    Added ability to record the screen

--- src/js/tools/share.js
+++ src/js/tools/share.js
@@ -1,5 +1,91 @@
 import { LitElement, html, css } from 'lit';
 
+
+
+
+let chunks = [];
+let mediaRecorder;
+
+const startRecord = async (videoElement) => {
+  const mimeType = 'video/webm;codecs=vp8,opus';
+
+  if (!MediaRecorder.isTypeSupported(mimeType)) {
+    alert('vp8/opus mime type is not supported');
+
+    return;
+  }
+
+  const options = {
+    audioBitsPerSecond: 128000,
+    mimeType,
+    videoBitsPerSecond: 2500000
+  }
+
+  const mediaStream = await startCapture({});
+  videoElement.srcObject = mediaStream;
+  mediaRecorder = new MediaRecorder(mediaStream, options);
+
+  setListeners();
+
+  mediaRecorder.start(1000);
+};
+
+const stopRecord = async () => {
+  if (!mediaRecorder) return;
+
+  mediaRecorder.stop();
+};
+
+const getLocalMediaStream = async () => {
+  const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  localVideo.srcObject = mediaStream;
+
+  return mediaStream;
+};
+
+const setListeners = () => {
+  mediaRecorder.ondataavailable = handleOnDataAvailable;
+  mediaRecorder.onstop = handleOnStop;
+};
+
+const handleOnStop = () => {
+  saveFile();
+
+  destroyListeners();
+  mediaRecorder = undefined;
+};
+
+const destroyListeners = () => {
+  mediaRecorder.ondataavailable = undefined;
+  mediaRecorder.onstop = undefined;
+};
+
+const handleOnDataAvailable = ({ data }) => {
+  if (data.size > 0) {
+    chunks.push(data);
+  }
+};
+
+const saveFile = () => {
+  const blob = new Blob(chunks);
+
+  const blobUrl = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+
+  link.style = 'display: none';
+  link.href = blobUrl;
+  link.download = 'recorded_file.webm';
+
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+
+  window.URL.revokeObjectURL(blobUrl);
+  chunks = [];
+};
+
+
+
 async function startCapture(displayMediaOptions) {
   let captureStream;
 
@@ -13,29 +99,19 @@ async function startCapture(displayMediaOptions) {
   return captureStream;
 }
 
-function processStream(stream, mediaSource) {
-    const mediaRecorder = new MediaRecorder(stream)
-    const videoBuffer = mediaSource.addSourceBuffer('video/webm;codecs=vp8');
-
-    mediaRecorder.ondataavailable = (data) => {
-        let fileReader = new FileReader();
-        let arrayBuffer;
-
-        fileReader.onloadend = () => {
-            arrayBuffer = fileReader.result;
-            videoBuffer.appendBuffer(arrayBuffer)
-        }
-        fileReader.readAsArrayBuffer(data.data);
 
 
+
+async function  selectFile() {
+                const folderHandle = await window.showDirectoryPicker();
+                const fileHandle = await folderHandle.getFileHandle(`video.webm`, {create: true});
+                    
+                const writableStream = await fileHandle.createWritable();
+
+                    // write our file
+                return writableStream;
+       
     }
-    mediaRecorder.start()
-
-    setInterval(() => {
-        mediaRecorder.requestData()
-    }, 1000)
-}
-
 
 
 export class Share extends LitElement {
@@ -53,11 +129,7 @@ export class Share extends LitElement {
     }
   
    async record() {
-      const video = this.shadowRoot.querySelector('video');
-      const mediaSource = new MediaSource();
-      video.src = URL.createObjectURL(mediaSource);
-      const stream = await startCapture({});
-      processStream(stream. mediaSource);
+     startRecord(this.shadowRoot.querySelector('video'));
    }
 }
 customElements.define('tc-share', Share);
