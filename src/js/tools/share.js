4c0d33b3ec88de25c316fe4f9b33daf53c5aa5c4
Tiberiu CORBU <tiberiu.corbu@gmail.com>
2023-05-17 13:26:54 +0200

    Try

--- src/js/tools/share.js
+++ src/js/tools/share.js
@@ -13,4 +13,61 @@ async function startCapture(displayMediaOptions) {
   return captureStream;
 }
 
+function processStream(stream, mediaSource) {
+    const mediaRecorder = new MediaRecorder(stream)
+    const videoBuffer = mediaSource.addSourceBuffer('video/webm;codecs=vp8');
 
+    mediaRecorder.ondataavailable = (data) => {
+        let fileReader = new FileReader();
+        let arrayBuffer;
+
+        fileReader.onloadend = () => {
+            arrayBuffer = fileReader.result;
+            videoBuffer.appendBuffer(arrayBuffer)
+        }
+        fileReader.readAsArrayBuffer(data.data);
+
+
+    }
+    mediaRecorder.start()
+
+    setInterval(() => {
+        mediaRecorder.requestData()
+    }, 1000)
+}
+
+import { LitElement, html, css } from 'lit';
+
+export class Cookie extends LitElement {
+    static properties = {
+        // cookieName: { state: true },
+        // dependenteCookieName: { state: true },
+        text: { state: true },
+        recording: {state: true}
+
+    };
+    static styles = css`
+        
+  
+    `
+
+    constructor() {
+        super()
+        
+    }
+
+    render() {
+        return html`
+            <video></video>
+            <button @click="${this.record}">Record Screen</button>
+        `
+    }
+   async record() {
+      const video = this.shadowRoot.querySelector('video');
+      const mediaSource = new MediaSource();
+      video.src = URL.createObjectURL(mediaSource);
+      const stream = await startCapture({});
+      processStream(stream. mediaSource);
+   }
+}
+customElements.define('tc-cookie', Cookie);
