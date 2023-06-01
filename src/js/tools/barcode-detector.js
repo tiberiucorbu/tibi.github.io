050160ed3ef99c0d6733eaaeee5111102d197f11
Tiberiu CORBU <tiberiu.corbu@gmail.com>
2023-06-01 09:20:40 +0200

    Show results

--- src/js/tools/barcode-detector.js
+++ src/js/tools/barcode-detector.js
@@ -8,15 +8,25 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-
 import {css, html, LitElement} from 'lit';
 
 export class BarcodeDetectorElement extends LitElement {
     static properties = {
         // text: { state: true },
-        results: {state: true, type: Array},
-        scanning: {state: true, type: Boolean}
+        results: {
+            state: true,
+            type: Array
+        },
+        scanning: {
+            state: true,
+            type: Boolean
+        }
     };
+
+    get video() {
+        return (this._video ??= this.renderRoot?.querySelector('video') ?? null);
+    }
+
     static styles = css`
        :host {
             display:block;
@@ -27,8 +37,8 @@ export class BarcodeDetectorElement extends LitElement {
             max-width: 960px;
             position: relative;
             margin: 0 auto;
-            grid-template-rows: 1fr auto;
-            grid-template-columns: auto;
+            grid-template-rows: 420px auto auto;
+            grid-template-columns: 1fr;
         }
 
         #buttons {
@@ -36,31 +46,38 @@ export class BarcodeDetectorElement extends LitElement {
           justify-content: center;
           padding: 16px;
         }
-
-        #results {
-          width: 100%;
+        #video-wrapper {
+           height: 100%;
+           position: relative; /* magic sauce II */
+        }
+        #video {
+              width: 100%;
+              height: 100%;
+              object-fit: cover;
+              position: absolute;
         }
 
-    `
+        #results {
+            width: 100%;
+        }
 
+    `;
     constructor() {
         super();
         this.results = [];
+        this.canvas = new OffscreenCanvas(1,1);
     }
 
     render() {
-        return ("BarcodeDetector" in window) ?
-            html`
+        return ("BarcodeDetector"in window) ? html`
                 <div id="wrapper">
-                    <div id="buttons">
-                        ${!this.scanning ?
-                                html`
-                                    <button @click="${this.scan}">⏺ Start Scanning</button>` :
-                                html`
-                                    <button @click="${this.stop}">⛔ Stop</button>`}
+                    <div id="video-wrapper">
+                      <video id="video"></video>
                     </div>
-                    <ul id="results">${this.results.map(result => html`
-                        <li>${result.rawValue}</li>`)}
+                    <div id="buttons">
+                        ${!this.scanning ? html`<button @click="${this.scan}">⏺ Start Scanning</button>` : html`<button @click="${this.stop}">⛔ Stop </button>`}
+                    </div>
+                    <ul id="results">${this.results.map(result=>html`<li>${result}</li>`)}
                 </div>
             ` : html`😭 Your browser doesn't support the barcode api`;
     }
@@ -69,49 +86,107 @@ export class BarcodeDetectorElement extends LitElement {
         this.scanning = true;
         // check supported types
         const formats = await BarcodeDetector.getSupportedFormats();
-        const barcodeDetector = new BarcodeDetector({formats});
+        const barcodeDetector = new BarcodeDetector({
+            formats
+        });
 
         this.stream = await navigator.mediaDevices.getUserMedia({
             video: {
                 facingMode: {
                     ideal: 'environment'
+                },
+                width: {
+                    min: 1024,
+                    ideal: 1280,
+                    max: 1920
+                },
+                height: {
+                    min: 576,
+                    ideal: 720,
+                    max: 1080
                 }
-            }, audio: false
+
+            },
+            audio: false
         });
 
         const videoTrack = this.stream.getVideoTracks()[0];
-        const trackProcessor = new MediaStreamTrackProcessor({track: videoTrack});
-        // const trackGenerator = new MediaStreamTrackGenerator({kind: "video"});
+        const {width, height} = videoTrack.getSettings();
+        this.canvas.width = width;
+        this.canvas.height = height;
+
+        const trackProcessor = new MediaStreamTrackProcessor({
+            track: videoTrack
+        });
+        const trackGenerator = new MediaStreamTrackGenerator({
+            kind: "video"
+        });
         const that = this;
         const transformer = new TransformStream({
-            async transform(videoFrame, controller) {
+            async transform(inputFrame, controller) {
                 try {
-                    const image = await createImageBitmap(videoFrame);
+                    const image = await createImageBitmap(inputFrame);
                     const barcodes = await barcodeDetector.detect(image);
-                    that.addResults(...barcodes);
-                    // const newFrame = highlightBarcodes(videoFrame, barcodes);
-                    videoFrame.close();
-                    // controller.enqueue(newFrame);
+                    let outputFrame;
+                    if (barcodes.length) {
+                        that.addResults(barcodes);
+                        outputFrame = await that.highlightBarcodes(image, inputFrame.timestamp, barcodes);
+                        inputFrame.close();
+                    } else {
+                        outputFrame = inputFrame;
+                    }
+                    controller.enqueue(outputFrame);
                 } catch (e) {
                     alert(e.message)
                 }
             },
+            flush(controller) {
+                controller.terminate();
+            }
         });
 
-        await trackProcessor.readable
-            .pipeThrough(transformer);
-        // .pipeTo(trackGenerator.writable);
+        this.outputStream = new MediaStream();
+        this.outputStream.addTrack(trackGenerator);
+        // currentProcessedStream = processedStream;
+        trackProcessor.readable.pipeThrough(transformer).pipeTo(trackGenerator.writable);
+        this.video.addEventListener('loadedmetadata', ()=>{
+                this.video.play()
+            }
+        )
+        this.video.srcObject = this.outputStream;
+
     }
 
     addResults(results) {
-        this.results.push(...results);
+        this.results.push(...results.map(result=>result.rawValue));
         this.results = [...new Set(this.results)];
         this.requestUpdate('results');
     }
 
+    async highlightBarcodes(bitmap, timestamp, barcodes) {
+        const ctx = this.canvas.getContext('2d');
+        const floor = Math.floor;
+        ctx.drawImage(bitmap, 0, 0, this.canvas.width, this.canvas.height);
+        bitmap.close();
+        barcodes.map(barcode=>{
+                const {x, y, width, height} = barcode.boundingBox;
+                ctx.strokeRect(floor(x), floor(y), floor(width), floor(height));
+                const text = barcode.rawValue;
+                const dimensions = ctx.measureText(text);
+                ctx.fillText(text, floor(x + width / 2 - dimensions.width / 2), floor(y) + height + 20);
+            }
+        );
+        const newBitmap = await createImageBitmap(this.canvas);
+        return new VideoFrame(newBitmap,{
+            timestamp
+        });
+    }
+
     stop() {
-        this.stream.getVideoTracks().forEach(track => track.stop());
+        this.stream.getVideoTracks().forEach(track=>track.stop());
+        this.outputStream.getVideoTracks().forEach(track=>track.stop());
         this.scanning = false;
+        this.video.stop();
     }
 
 }
